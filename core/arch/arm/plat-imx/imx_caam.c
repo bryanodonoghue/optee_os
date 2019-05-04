cb89e00fc3fa60c0927ef3530fdaea2ba4ba5299
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2019-05-05 00:45:17 +0100

    core: imx: Add simple CAAM permissions set routine
    When we transition to secure-world certain parts of the CAAM become opaque
    to normal world. This patch adds a simple routine to set CAAM job-ring
    permissions to normal-world by default.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ core/arch/arm/plat-imx/imx_caam.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (C) 2019 Bryan O'Donoghue
+ *
+ * Bryan O'Donoghue <bryan.odonoghue@linaro.org>
+ */
+
+#include <io.h>
+#include <imx_caam.h>
+#include <kernel/generic_boot.h>
+#include <platform_config.h>
+#include <stdint.h>
+
+void init_caam(void)
+{
+	struct imx_caam_ctrl *caam = (struct caam_ctrl *)CAAM_BASE;
+	uint32_t reg;
+	int i;
+
+	for (i = 0; i < CAAM_NUM_JOB_RINGS; i++) {
+		reg = io_read32(&caam->jr[i].jrmidr_ms);
+		reg |= JROWN_NS | JROWN_MID;
+		io_write32(&caam->jr[i].jrmidr_ms, reg);
+	}
+}
