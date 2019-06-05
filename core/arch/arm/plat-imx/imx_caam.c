bryanodonoghue/optee_os

commit 4cb61ae7d98ee089f78d71fbaad2cecbddf6ba7e

core: imx: Add simple CAAM permissions set routine
When we transition to secure-world certain parts of the CAAM become opaque
to normal world. This patch adds a simple routine to set CAAM job-ring
permissions to normal-world by default.

Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

core/arch/arm/plat-imx/imx_caam.c
@@ -1,0 +1,40 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (C) 2019 Bryan O'Donoghue
+ *
+ * Bryan O'Donoghue <bryan.odonoghue@linaro.org>
+ */
+
+#include <io.h>
+#include <imx_caam.h>
+#include <kernel/generic_boot.h>
+#include <platform_config.h>
+#include <stdint.h>
+
+void init_caam(void)
+{
+	struct imx_caam_ctrl *caam = (struct caam_ctrl *)CAAM_BASE;
+	uint32_t reg;
+	int i;
+
+	/*
+	 * Set job-ring ownership to non-secure by default.
+	 * A Linux kernel that runs after OP-TEE will run in normal-world
+	 * so we want to enable that kernel to have total ownership of the
+	 * CAAM job-rings.
+	 *
+	 * It is possible to use CAAM job-rings inside of OP-TEE i.e. in
+	 * secure world code but, to do that OP-TEE and kernel should agree
+	 * via a DTB which job-rings are owned by OP-TEE and which are
+	 * owned by Kernel, something that the OP-TEE CAAM driver should
+	 * set up.
+	 *
+	 * This code below simply sets a default for the case where no
+	 * runtime OP-TEE CAAM code will be run
+	 */
+	for (i = 0; i < CAAM_NUM_JOB_RINGS; i++) {
+		reg = io_read32((vaddr_t)&caam->jr[i].jrmidr_ms);
+		reg |= JROWN_NS | JROWN_MID;
+		io_write32((vaddr_t)&caam->jr[i].jrmidr_ms, reg);
+	}
+}

core/arch/arm/plat-imx/imx_caam.h
@@ -1,0 +1,36 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2019 Bryan O'Donoghue
+ *
+ * Bryan O'Donoghue <bryan.odonoghue@linaro.org>
+ */
+
+#ifndef __IMX_CAAM_H__
+#define __IMX_CAAM_H__
+
+#include <imx-regs.h>
+#include <stdint.h>
+
+struct imx_caam_job_ring {
+	uint32_t			jrmidr_ms;
+	uint32_t			jrmidr_ls;
+};
+
+#define CAAM_NUM_JOB_RINGS		4
+
+/* CAAM ownersip definition bits */
+#define JROWN_NS			BIT(3)
+#define JROWN_MID			0x01
+
+/* A basic sub-set of the CAAM */
+struct imx_caam_ctrl {
+	uint32_t			res0;
+	uint32_t			mcfgr;
+	uint32_t			res1;
+	uint32_t			scfgr;
+	struct imx_caam_job_ring	jr[CAAM_NUM_JOB_RINGS];
+};
+
+void init_caam(void);
+
+#endif /* __IMX_CAAM_H__ */

commit db4657e24d6dff46e0579c0b39142ed485ade80e

core: imx: Fix compilation warning leading to build breakage
Commit 4cb61ae7d98e ("core: imx: Add simple CAAM permissions set routine")
has the following cast, which has a typo resulting in a forward declaration
of "struct caam_ctrl" which is not in fact a structure defined anywhere.

struct imx_caam_ctrl *caam = (struct caam_ctrl *)CAAM_BASE;

It looks like it shouldn't compile but, it does.

Fixes: 4cb61ae7d98e ("core: imx: Add simple CAAM permissions set routine")

Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
Reported-by: Jerome Forissier <jerome@forissier.org>

core/arch/arm/plat-imx/imx_caam.c
@@ -13,7 +13,7 @@
 
 void init_caam(void)
 {
-	struct imx_caam_ctrl *caam = (struct caam_ctrl *)CAAM_BASE;
+	struct imx_caam_ctrl *caam = (struct imx_caam_ctrl*)CAAM_BASE;
 	uint32_t reg;
 	int i;
 